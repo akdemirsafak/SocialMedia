13436feff8208504605c7448c33515ffe449f23f
akdemirsafak <akdemirsafak@gmail.com>
2024-02-20 02:18:04 +0300

    Add project files.

---
+++ AspNetCore/1-SwitchExpression_Net8/1-SwitchExpression_Net8/Program.cs
@@ -0,0 +1,39 @@
+﻿int number=5;
+
+//Before .Net 8
+
+switch(number)
+{
+
+    case 1:
+        Console.WriteLine("One");
+        break;
+    case 2:
+        Console.WriteLine("Two");
+        break;
+    case 3:
+        Console.WriteLine("Three");
+        break;
+    case 4:
+        Console.WriteLine("Four");
+        break;
+    case 5:
+        Console.WriteLine("Five");
+        break;
+    default:
+        Console.WriteLine("Unknown");
+        break;
+}
+
+
+//After .Net 8
+
+string result = number switch
+{
+    1 => "One",
+    2 => "Two",
+    3 => "Three",
+    4 => "Four",
+    5 => "Five",
+    _ => "Unknown"
+};
