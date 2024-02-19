f4c10c3ec36dd904ca8b3d540b7b81a30ee9d94b
akdemirsafak <akdemirsafak@gmail.com>
2024-02-20 02:36:55 +0300

    CultureInfo

---
+++ AspNetCore/2-CultureInfo_For_ToUpper_ToLower/2-CultureInfo_For_ToUpper_ToLower/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//BAD 
+using System.Globalization;
+
+Thread.CurrentThread.CurrentCulture = new CultureInfo("tr-TR");
+string turkishWord="insteresting";
+bool isEquals = turkishWord.ToUpper() == "INTERESTING"; //false
+
+//TRUE - GOOD
+Thread.CurrentThread.CurrentCulture = new CultureInfo("tr-TR");
+string turkishWord2="insteresting";
+bool isEqual=string.Equals(turkishWord,"INTERESTING",StringComparison.OrdinalIgnoreCase); //true
